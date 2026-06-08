4f20dffea85dfe85d611e432ff7c818b96bd65d1
Sebastian Cid <sebastiancid@MacBook-Air-de-Sebastian-2.local>
2026-06-07 21:25:59 -0400

    Show sync status and manual refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "stracon-docs-v4";
-const RUNTIME_CACHE = "stracon-docs-runtime-v4";
+const CACHE_NAME = "stracon-docs-v5";
+const RUNTIME_CACHE = "stracon-docs-runtime-v5";
 const APP_SHELL = [
   "./",
   "./simple.html",
